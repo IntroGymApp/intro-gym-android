29b114fa14fff86a9d17fad7a1fa1eb1c4cd8419
lonelywh1te <lonelywh1te@ya.ru>
2025-02-26 21:49:21 +1000

    tests(auth): Add validator tests with confirm password

--- app/src/test/java/ru/lonelywh1te/introgym/features/auth/domain/EmailPasswordValidatorTest.kt
+++ app/src/test/java/ru/lonelywh1te/introgym/features/auth/domain/EmailPasswordValidatorTest.kt
@@ -10,9 +10,36 @@ class EmailPasswordValidatorTest {
     private val validator = EmailPasswordValidator
 
     @Nested
+    inner class ValidateEmailWithConfirmPasswordTests() {
+
+        @Test
+        fun `validate with correct email and similar password returns Success`() {
+            val email = "test@example.com"
+            val password = "TestPassword123!"
+
+            val expected = Result.Success(Unit)
+            val actual = validator.validate(email, password, password)
+
+            Assertions.assertEquals(expected, actual)
+        }
+
+        @Test
+        fun `validate with correct email and different password returns PASSWORD_MISMATCH`() {
+            val email = "test@example.com"
+            val password = "TestPassword123!"
+            val confirmPassword = "Testpassword123"
+
+            val expected = Result.Failure(ValidationError.PASSWORD_MISMATCH)
+            val actual = validator.validate(email, password, confirmPassword)
+
+            Assertions.assertEquals(expected, actual)
+        }
+    }
+
+    @Nested
     inner class ValidateEmailAndPasswordTests() {
         @Test
-        fun `validate with correct email and password`() {
+        fun `validate with correct email and password returns Success`() {
             val email = "test@example.com"
             val password = "TestPassword123!"
 
@@ -23,7 +50,7 @@ class EmailPasswordValidatorTest {
         }
 
         @Test
-        fun `validate with incorrect email and password`() {
+        fun `validate with incorrect email and password returns INVALID_EMAIL_FORMAT`() {
             val email = "tes t@example_com"
             val password = "reff fds"
 
@@ -34,7 +61,7 @@ class EmailPasswordValidatorTest {
         }
 
         @Test
-        fun `validate with incorrect email and correct password`() {
+        fun `validate with incorrect email and correct password returns INVALID_EMAIL_FORMAT`() {
             val email = "tes t@example_com"
             val password = "TestPassword123!"
 
@@ -45,7 +72,7 @@ class EmailPasswordValidatorTest {
         }
 
         @Test
-        fun `validate with correct email and incorrect password`() {
+        fun `validate with correct email and incorrect password returns INVALID_PASSWORD_FORMAT`() {
             val email = "test@example.com"
             val password = "reff fds"
 
@@ -56,7 +83,7 @@ class EmailPasswordValidatorTest {
         }
 
         @Test
-        fun `validate with correct email and empty password`() {
+        fun `validate with correct email and empty password returns INVALID_PASSWORD_FORMAT`() {
             val email = "test@example.com"
             val password = ""
 
@@ -70,7 +97,7 @@ class EmailPasswordValidatorTest {
     @Nested
     inner class ValidateEmailTests() {
         @Test
-        fun `validate with correct email`() {
+        fun `validate with correct email returns Success`() {
             val email = "test@example.com"
 
             val expected = Result.Success(Unit)
@@ -80,7 +107,7 @@ class EmailPasswordValidatorTest {
         }
 
         @Test
-        fun `validate with incorrect email`() {
+        fun `validate with incorrect email returns INVALID_EMAIL_FORMAT`() {
             val email = "tes t@example_com"
 
             val expected = Result.Failure(ValidationError.INVALID_EMAIL_FORMAT)
