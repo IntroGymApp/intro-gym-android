bae80b8036a1a1c688f8b71e703c4981ec584c38
lonelywh1te <lonelywh1te@ya.ru>
2025-05-06 18:49:03 +1000

    fix(home): Workout log items sorting

--- app/src/main/java/ru/lonelywh1te/introgym/features/home/domain/usecase/GetWorkoutLogItemListUseCase.kt
+++ app/src/main/java/ru/lonelywh1te/introgym/features/home/domain/usecase/GetWorkoutLogItemListUseCase.kt
@@ -1,8 +1,10 @@
 package ru.lonelywh1te.introgym.features.home.domain.usecase
 
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.map
 import ru.lonelywh1te.introgym.core.result.Result
 import ru.lonelywh1te.introgym.features.home.domain.models.WorkoutLogItem
+import ru.lonelywh1te.introgym.features.home.domain.models.WorkoutLogState
 import ru.lonelywh1te.introgym.features.home.domain.repository.WorkoutLogRepository
 import java.time.LocalDate
 
@@ -10,6 +12,22 @@ class GetWorkoutLogItemListUseCase(
     private val repository: WorkoutLogRepository,
 ) {
     operator fun invoke(date: LocalDate): Flow<Result<List<WorkoutLogItem>>> {
-        return repository.getWorkoutLogItemList(date)
+        return repository.getWorkoutLogItemList(date).map { result ->
+            when(result) {
+                is Result.Success -> Result.Success(sortWorkoutLogItems(result.data))
+                is Result.Failure -> result
+                is Result.Loading -> result
+            }
+        }
+    }
+
+    private fun sortWorkoutLogItems(workoutLogItems: List<WorkoutLogItem>): List<WorkoutLogItem> {
+        return workoutLogItems.sortedBy { item ->
+            when (item.state) {
+                WorkoutLogState.InProgress -> 0
+                WorkoutLogState.NotStarted -> 1
+                WorkoutLogState.Finished -> 2
+            }
+        }
     }
 }
