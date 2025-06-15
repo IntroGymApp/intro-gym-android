96b9b036a4049f745f07dbbfbf04cf5c7aba6b8d
lonelywh1te <lonelywh1te@ya.ru>
2025-06-16 07:43:00 +1000

    chore(*): Change version from 1.0.0 to 1.0.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -17,7 +17,7 @@ android {
 
         val major = 1
         val minor = 0
-        val patch = 0
+        val patch = 1
 
         versionCode = major * 10000 + minor * 100 + patch
         versionName = "$major.$minor.$patch"
